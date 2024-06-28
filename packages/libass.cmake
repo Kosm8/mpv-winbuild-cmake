cf7f918069ba88379749a2dc3193ec491bd2362c
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2024-06-28 23:02:11 +0200

    libass: apply multithreaded patch

--- packages/libass.cmake
+++ packages/libass.cmake
@@ -8,23 +8,20 @@ ExternalProject_Add(libass
         libunibreak
     GIT_REPOSITORY https://github.com/libass/libass.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_CLONE_FLAGS ""
+    PATCH_COMMAND ${EXEC} curl -sL https://github.com/libass/libass/pull/793.patch | git am --3way --whitespace=fix
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
+    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
-        --libdir=${MINGW_INSTALL_PREFIX}/lib
-        --cross-file=${MESON_CROSS}
-        --buildtype=release
-        --default-library=static
-        -Dfontconfig=enabled
-        -Ddirectwrite=enabled
-        -Dasm=enabled
-        -Dlibunibreak=enabled
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
-    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+        --disable-shared
+        --disable-pthreads
+        CFLAGS='-Wno-error=int-conversion'
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    BUILD_IN_SOURCE 1
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_PATCH 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libass)
-force_meson_configure(libass)
 cleanup(libass install)
