b4f542c904dfdd02c47ec29c54db810a0cbcb055
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2024-07-13 05:25:46 +0200

    libass: apply multithreaded patch

--- packages/libass.cmake
+++ packages/libass.cmake
@@ -6,25 +6,22 @@ ExternalProject_Add(libass
         libiconv
         fontconfig
         libunibreak
-    GIT_REPOSITORY https://github.com/libass/libass.git
+    GIT_REPOSITORY https://github.com/rcombs/libass.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG threading
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
+    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
-        --libdir=${MINGW_INSTALL_PREFIX}/lib
-        --cross-file=${MESON_CROSS}
-        --buildtype=release
-        --default-library=static
-        -Dfontconfig=enabled
-        -Ddirectwrite=enabled
-        -Dasm=enabled
-        -Dlibunibreak=enabled
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
-    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+        --disable-shared
+        CFLAGS='-Wno-error=int-conversion'
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    BUILD_IN_SOURCE 1
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_PATCH 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libass)
-force_meson_configure(libass)
 cleanup(libass install)
