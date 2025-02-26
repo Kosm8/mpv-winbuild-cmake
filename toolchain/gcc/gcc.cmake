54822825c56fa7c15a7cd60f8c38d3c867a4a056
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2025-02-26 12:59:40 +0100

    fork

--- toolchain/gcc/gcc.cmake
+++ toolchain/gcc/gcc.cmake
@@ -1,28 +1,32 @@
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/14-20251205/gcc-14-20251205.tar.xz
-    # https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/13-20240309/sha512.sum
-    URL_HASH SHA512=b05c932de5c478cf9b64a4f2cbdf1de9b37a4845614ff8bc0e0563cd1e716f69ea0fe070ef9bf5ec1fccc0658705ec76cef536ac8dc4ca4c32cf81dad8588c37
-    DOWNLOAD_DIR ${SOURCE_LOCATION}
+    GIT_REPOSITORY https://github.com/Kosm8/gcc.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_TAG releases/gcc-15
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
         --prefix=${CMAKE_INSTALL_PREFIX}
         --libdir=${CMAKE_INSTALL_PREFIX}/lib
+        --without-included-gettext
         --with-sysroot=${CMAKE_INSTALL_PREFIX}
         --program-prefix=cross-
+        --disable-checking
+        --disable-libgomp
         --disable-multilib
-        --enable-languages=c,c++
         --disable-nls
         --disable-shared
-        --disable-win32-registry
-        --with-arch=${GCC_ARCH}
-        --with-tune=generic
-        --enable-threads=posix
-        --without-included-gettext
-        --enable-lto
-        --enable-checking=release
         --disable-sjlj-exceptions
+        --disable-win32-registry
+        --enable-default-pie
+        --enable-host-bind-now
+        --enable-host-pie
+        --enable-languages=c,c++
+        --enable-libstdcxx-threads=yes
+        --enable-lto
+        --enable-threads=win32
     BUILD_COMMAND make -j${MAKEJOBS} all-gcc
     INSTALL_COMMAND make install-strip-gcc
     STEP_TARGETS download install
@@ -42,4 +46,5 @@ ExternalProject_Add_Step(gcc final
     LOG 1
 )
 
+force_rebuild_git(gcc)
 cleanup(gcc final)
