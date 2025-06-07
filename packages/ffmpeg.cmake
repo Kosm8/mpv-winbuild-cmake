086f1d570c9c119d81bf9f00ec5e9f5748168149
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2025-06-07 05:40:19 +0200

    t

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -15,7 +15,7 @@ ExternalProject_Add(ffmpeg
         libzvbi
         ${nvcodec_headers}
         shaderc
-    GIT_REPOSITORY https://github.com/librempeg/librempeg.git
+    GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
